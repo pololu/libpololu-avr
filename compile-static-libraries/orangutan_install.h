8dbce7efd06332d33ab3e817bc945e8b4da018a3
Paul (Pololu) <dev-paul@pololu.com>
2008-05-26 03:25:31 +0000

    added installable orangutan.h
    git-svn-id: svn+ssh://subversion/lib-orangutan@8 8500f93d-0007-49e4-9d50-5eee3f077541

---
+++ compile-static-libraries/orangutan_install.h
@@ -0,0 +1,6 @@
+#include <orangutan/motors.h>
+#include <orangutan/buzzer.h>
+#include <orangutan/pushbuttons.h>
+#include <orangutan/lcd.h>
+#include <orangutan/delay.h>
+#include <orangutan/leds.h>
