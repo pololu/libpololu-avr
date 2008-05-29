5ee4faa5bb7c65f3867782136c140a0e1a9521c7
Paul (Pololu) <dev-paul@pololu.com>
2008-05-29 05:54:44 +0000

    changed to libpololu
    git-svn-id: svn+ssh://subversion/lib-orangutan@51 8500f93d-0007-49e4-9d50-5eee3f077541

--- compile-static-libraries/orangutan_install.h
+++
@@ -1,8 +0,0 @@
-#include <orangutan/analog.h>
-#include <orangutan/buzzer.h>
-#include <orangutan/delay.h>
-#include <orangutan/motors.h>
-#include <orangutan/lcd.h>
-#include <orangutan/leds.h>
-#include <orangutan/line_sensors.h>
-#include <orangutan/pushbuttons.h>
---
+++ compile-static-libraries/pololu_install.h
@@ -0,0 +1,8 @@
+#include <pololu/analog.h>
+#include <pololu/buzzer.h>
+#include <pololu/delay.h>
+#include <pololu/motors.h>
+#include <pololu/lcd.h>
+#include <pololu/leds.h>
+#include <pololu/line_sensors.h>
+#include <pololu/pushbuttons.h>
