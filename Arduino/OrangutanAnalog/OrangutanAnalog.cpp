55117554027aa34bd236207e61d0f66f6ece6f80
Paul (Pololu) <dev-paul@pololu.com>
2008-06-02 18:08:18 +0000

    Added a license
    git-svn-id: svn+ssh://subversion/lib-orangutan@84 8500f93d-0007-49e4-9d50-5eee3f077541

--- Arduino/OrangutanAnalog/OrangutanAnalog.cpp
+++ Arduino/OrangutanAnalog/OrangutanAnalog.cpp
@@ -2,15 +2,25 @@
   OrangutanAnalog.cpp - Library for using the handling analog inputs on the
 	Orangutan LV-168 and Baby Orangutan B.  This library also provides a
 	method for reading the temperature sensor on the LV-168.
-  Written by Ben Schmidel, May 27, 2008.
-  http://www.pololu.com
-  http://forum.pololu.com
-  Released into the public domain.
-  
-  Disclaimer: To the extent permitted by law, Pololu Corporation provides
-  this work without any warranty.  It may be defective, in which case you
-  agree to be responsible for all resulting costs and damages.
 */
+
+/*
+ * Written by Ben Schmidel, May 27, 2008.
+ * Copyright (c) 2008 Pololu Corporation. For more information, see
+ *
+ *   http://www.pololu.com
+ *   http://forum.pololu.com
+ *
+ * You may freely modify and share this code, as long as you keep this
+ * notice intact (including the two links above).  Licensed under the
+ * Creative Commons BY-SA 3.0 license:
+ *
+ *   http://creativecommons.org/licenses/by-sa/3.0/
+ *
+ * Disclaimer: To the extent permitted by law, Pololu provides this work
+ * without any warranty.  It may be defective, in which case you agree
+ * to be responsible for all resulting costs and damages.
+ */
 
 #include <avr/io.h>
 #include "OrangutanAnalog.h"
