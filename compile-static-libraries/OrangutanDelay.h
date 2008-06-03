a8be1905a5dffc0e8a2bd0bbbc8291f6ba4816fe
Paul (Pololu) <dev-paul@pololu.com>
2008-06-03 22:25:25 +0000

    various updates and improvements to the C library
    git-svn-id: svn+ssh://subversion/lib-orangutan@96 8500f93d-0007-49e4-9d50-5eee3f077541

---
+++ compile-static-libraries/OrangutanDelay.h
@@ -0,0 +1,19 @@
+#ifndef OrangutanDelay_h
+#define OrangutanDelay_h
+
+class OrangutanDelay
+{
+  public:
+	// Delays the specified number of milliseconds, which can be any
+	// integer value.
+	static void ms(unsigned int milliseconds);
+};
+
+#endif
+
+// Local Variables: **
+// mode: C++ **
+// c-basic-offset: 4 **
+// tab-width: 4 **
+// indent-tabs-mode: t **
+// end: **
