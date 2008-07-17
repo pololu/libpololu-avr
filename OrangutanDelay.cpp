4b571925bbc3df6992c31838f197d4b9a61f9268
Paul (Pololu) <dev-paul@pololu.com>
2008-07-17 20:25:47 +0000

    used our us function
    git-svn-id: svn+ssh://subversion/lib-orangutan@183 8500f93d-0007-49e4-9d50-5eee3f077541

--- OrangutanDelay.cpp
+++ OrangutanDelay.cpp
@@ -5,8 +5,6 @@
 #define F_CPU 20000000UL
 #include "OrangutanDelay.h"
 
-#include <avr/delay.h>
-
 extern "C" void delay_ms(unsigned int milliseconds)
 {
   OrangutanDelay::ms(milliseconds);
@@ -17,5 +15,5 @@ void OrangutanDelay::ms(unsigned int milliseconds)
   unsigned int i;
 
   for ( i = 0; i < milliseconds; i++ )
-    _delay_us( 1 );
+    us( 1000 );
 }
