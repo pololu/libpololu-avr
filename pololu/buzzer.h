bf2ccb40a314704220cdee70733b5541cc45b020
Paul (Pololu) <dev-paul@pololu.com>
2008-07-17 02:35:45 +0000

    Added buzzer functions
    git-svn-id: svn+ssh://subversion/lib-orangutan@173 8500f93d-0007-49e4-9d50-5eee3f077541

--- pololu/buzzer.h
+++ pololu/buzzer.h
@@ -14,6 +14,30 @@
 #define PLAY_AUTOMATIC 0
 #define PLAY_CHECK 1
 
+#define C(x)			( 0 + (x)*12)
+#define C_SHARP(x)		( 1 + (x)*12)
+#define D_FLAT(x)		( 1 + (x)*12)
+#define D(x)			( 2 + (x)*12)
+#define D_SHARP(x)		( 3 + (x)*12)
+#define E_FLAT(x)		( 3 + (x)*12)
+#define E(x)			( 4 + (x)*12)
+#define F(x)			( 5 + (x)*12)
+#define F_SHARP(x)		( 6 + (x)*12)
+#define G_FLAT(x)		( 6 + (x)*12)
+#define G(x)			( 7 + (x)*12)
+#define G_SHARP(x)		( 8 + (x)*12)
+#define A_FLAT(x)		( 8 + (x)*12)
+#define A(x)			( 9 + (x)*12)
+#define A_SHARP(x)		(10 + (x)*12)
+#define B_FLAT(x)		(10 + (x)*12)
+#define B(x)			(11 + (x)*12)
+
+// special notes
+#define A4				A(4)			// center of the Equal-Tempered Scale
+#define SILENT_NOTE		0xFF			// this note will silence the buzzer
+
+#define DIV_BY_10		(1 << 15)		// frequency bit that indicates Hz/10
+
 void buzzer_init();
 void play_frequency(unsigned int freq, unsigned int duration, 
 		       unsigned char volume);
