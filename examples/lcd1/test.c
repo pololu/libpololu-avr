42adba008e7e860dcfa6dfe36f6e09c5e28442a1
Paul (Pololu) <dev-paul@pololu.com>
2008-07-17 22:17:04 +0000

    git-svn-id: svn+ssh://subversion/lib-orangutan@189 8500f93d-0007-49e4-9d50-5eee3f077541

---
+++ examples/lcd1/test.c
@@ -0,0 +1,29 @@
+#include <pololu/orangutan.h>
+
+/*
+ * OrangutanLCDExample for the Orangutan LV-168
+ *
+ * This example program is indended for use on the Orangutan LV-168.
+ * It uses the OrangutanLCD library to display things on the LCD.
+ *
+ * http://www.pololu.com/docs/0J17/5.c
+ * http://www.pololu.com
+ * http://forum.pololu.com
+ */
+
+int main()                     // run over and over again
+{
+  while(1)
+  {
+    print("Hello");           // display "Hello" at (0, 0), a.k.a. upper-left
+    delay_ms(200);
+    lcd_scroll(LCD_RIGHT, 3, 200);// shift the display right every 200ms three times
+    clear();                  // clear the LCD
+    lcd_goto_xy(3, 1);             // go to the fourth character of the second LCD line
+    print("Hello");           // display "Hello" at (3, 1), a.k.a. lower-right
+    delay_ms(200);
+    lcd_scroll(LCD_LEFT, 3, 200); // shift the display left every 200ms three times
+    clear();                  // clear the LCD
+  }
+}
+
---
+++ examples/lcd2/test.c
@@ -0,0 +1,111 @@
+#include <pololu/orangutan.h>
+
+// get random functions
+#include <stdlib.h>
+
+/*
+ * lcd2: for the Orangutan LV-168
+ *
+ * This example program is indended for use on the Orangutan LV-168.
+ * It uses the OrangutanLCD functions to display custom characters on 
+ * the LCD.  Simply push a any user pushbutton to display a new,
+ * randomly chosen, custom mood characer.
+ *
+ * http://www.pololu.com/docs/0J17/5.c
+ * http://www.pololu.com
+ * http://forum.pololu.com
+ */
+
+// define some custom "mood" characters
+#include <avr/pgmspace.h>  // this lets us refer to data in program space (i.e. flash)
+const char happy[] PROGMEM = {
+  0b00000,                 // the five bits that make up the top row of the 5x8 character
+  0b01010,
+  0b01010,
+  0b01010,
+  0b00000,
+  0b10001,
+  0b01110,
+  0b00000
+};
+
+const char sad[] PROGMEM = {
+  0b00000, 
+  0b01010,
+  0b01010,
+  0b01010,
+  0b00000,
+  0b01110,
+  0b10001,
+  0b00000
+};
+
+const char indifferent[] PROGMEM = {
+  0b00000, 
+  0b01010,
+  0b01010,
+  0b01010,
+  0b00000,
+  0b00000,
+  0b01110,
+  0b00000
+};
+
+const char surprised[] PROGMEM = {
+  0b00000, 
+  0b01010,
+  0b01010,
+  0b00000,
+  0b01110,
+  0b10001,
+  0b10001,
+  0b01110
+};
+
+const char mocking[] PROGMEM = {
+  0b00000, 
+  0b01010,
+  0b01010,
+  0b01010,
+  0b00000,
+  0b11111,
+  0b00101,
+  0b00010
+};
+
+char prevMood = 5;
+
+
+int main()                    // run once, when the sketch starts
+{
+  lcd_load_custom_character(happy, 0);
+  lcd_load_custom_character(sad, 1);
+  lcd_load_custom_character(indifferent, 2);
+  lcd_load_custom_character(surprised, 3);
+  lcd_load_custom_character(mocking, 4);
+  clear();                  // this must be called before we can use the custom characters
+  print("mood: ?");
+
+  // initialize the random number generator based on how long they hold the button the first time
+  wait_for_button_press(ALL_BUTTONS);
+  long seed = 0;
+  while(button_is_pressed(ALL_BUTTONS))
+    seed++;
+  srandom(seed);
+
+  while(1)
+  {
+  
+    lcd_goto_xy(6, 0);             // move cursor to the correct position
+  
+    char mood;
+    do
+    {
+      mood = random()%5;
+    } while (mood == prevMood);   // ensure we get a new mood that differs from the previous
+    prevMood = mood;
+  
+    print_character(mood);   // print a random mood character
+    wait_for_button(ALL_BUTTONS);   // wait for any button to be pressed
+  }
+}
