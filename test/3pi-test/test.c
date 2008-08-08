f5bdb4b2e5c8e684096599de790054071d142a72
Paul (Pololu) <dev-paul@pololu.com>
2008-08-08 01:49:33 +0000

    added eeprom test
    git-svn-id: svn+ssh://subversion/lib-orangutan@237 8500f93d-0007-49e4-9d50-5eee3f077541

--- test/3pi-test/test.c
+++ test/3pi-test/test.c
@@ -6,6 +6,7 @@
 #include "pot.h"
 
 #include <avr/pgmspace.h>
+#include <avr/eeprom.h>
 
 // Introductory messages.
 const char welcome[] PROGMEM = ">g32>>c32";
@@ -57,9 +58,42 @@ int main()
 	pololu_3pi_init(1000);
 	lcd_init_printf();
 
-	play_from_program_space(welcome);
-	print_two_lines_delay_1s(welcome_line1,welcome_line2);
-	print_two_lines_delay_1s(demo_name_line1,demo_name_line2);
+	// test the reset button five times using EEPROM
+	int reset_count = eeprom_read_byte(0);
+
+	if(reset_count > 10)
+		reset_count = 0;
+
+	if(reset_count == 0)
+	{
+		play_from_program_space(welcome);
+		print_two_lines_delay_1s(welcome_line1,welcome_line2);
+		print_two_lines_delay_1s(demo_name_line1,demo_name_line2);
+	}
+
+	clear();
+
+	if(reset_count < 5)
+	{
+		print("Press");
+		lcd_goto_xy(0,1);
+		print("Reset ");
+		print_long(reset_count);
+		reset_count ++;
+		eeprom_write_byte(0,reset_count);
+		while(1);
+	}
+	else if(reset_count < 10)
+	{
+		print("Press");
+		lcd_goto_xy(0,1);
+		print("Power ");
+		print_long(reset_count-5);
+		reset_count ++;
+		eeprom_write_byte(0,reset_count);
+		while(1);
+	}
+	eeprom_write_byte(0,0);
 
 	printf("\nAssert");
 	assert(1 == 1); // make sure assert works
