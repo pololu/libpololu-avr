0c184aaf4c114e628be0d8c3d0d2fafa4111e012
Paul (Pololu) <dev-paul@pololu.com>
2009-04-04 02:38:04 +0000

    made makefiles for different devices
    git-svn-id: svn+ssh://subversion/lib-orangutan@345 8500f93d-0007-49e4-9d50-5eee3f077541

--- examples/simple-test/test.c
+++ examples/simple-test/test.c
@@ -2,24 +2,8 @@
 
 int main()
 {
-	print("Hello!");
-
-	play("L16 ceg>c");
-
-	while(1)
-	{
-		red_led(0);
-		green_led(1);
-
-		delay_ms(100);
-
-		red_led(1);
-		green_led(0);
-		
-		delay_ms(100);
-	}
-
-	return 0;
+	time_reset();
+	//	set_motors(0,0);
 }
 
 // Local Variables: **
