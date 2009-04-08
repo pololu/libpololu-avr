6a8db0d029c5cec5bbd5de137af5607ba078115e
Paul (Pololu) <dev-paul@pololu.com>
2009-04-08 06:13:21 +0000

    added new functions to analog
    git-svn-id: svn+ssh://subversion/lib-orangutan@350 8500f93d-0007-49e4-9d50-5eee3f077541

--- pololu/analog.h
+++ pololu/analog.h
@@ -8,10 +8,19 @@ unsigned char get_analog_mode();
 unsigned int analog_read(unsigned char channel);
 unsigned int analog_read_average(unsigned char channel, unsigned int samples);
 unsigned int read_trimpot();
+
 int read_temperature_f();
 int read_temperature_c();
-unsigned int read_battery_millivolts();
+
 void start_analog_conversion(unsigned char channel);
 unsigned char analog_is_converting();
 unsigned int analog_conversion_result();
 unsigned int to_millivolts(unsigned int analog_result);
+
+unsigned int read_battery_millivolts_3pi();
+inline unsigned int read_battery_millivolts()
+{
+  return read_battery_millivolts_3pi();
+}
+unsigned int read_battery_millivolts_sv168();
+
