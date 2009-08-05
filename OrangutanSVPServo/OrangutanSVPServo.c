be0b9a69c9ab4e633f09437ea37d5dcdb02d2533
Ben (Pololu) <dev-ben@pololu.com>
2009-08-05 21:29:17 +0000

    git-svn-id: svn+ssh://subversion/lib-orangutan@401 8500f93d-0007-49e4-9d50-5eee3f077541

--- OrangutanSVPServo/OrangutanSVPServo.c
+++ OrangutanSVPServo/OrangutanSVPServo.c
@@ -14,30 +14,36 @@ unsigned int servoTargetPos[8] 		= {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned int servoSpeed[8]			= {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char idx;
 
-struct PinStruct
+struct PortStruct
 {
-	unsigned char* pinRegister;
+	unsigned char* portRegister;
 	unsigned char bitmask;
 };
-struct PinStruct muxSelectors[3];
+#if defined (__AVR_ATmega324P__) || defined (__AVR_ATmega1284P__)
+struct PortStruct portPin[3];		// one pin for each mux selector pin
+#else
+struct PortStruct portPin[8];		// one pin for each servo
+#endif
 
 
 // This interrupt is executed when timer 1 = TOP (ICR1) and the value in OCR1B (the next duty cycle)
 // has been loaded.
 ISR(TIMER1_CAPT_vect)
 {
-	*(muxSelectors[0].pinRegister) ^= muxSelectors[0].bitmask;
+#if defined (__AVR_ATmega324P__) || defined (__AVR_ATmega1284P__)
+	*(portPin[0].portRegister) ^= portPin[0].bitmask;
 	if (idx & 1)
 	{
-		*(muxSelectors[1].pinRegister) ^= muxSelectors[1].bitmask;
+		*(portPin[1].portRegister) ^= portPin[1].bitmask;
 		if (idx & 2)
 		{
-			*(muxSelectors[2].pinRegister) ^= muxSelectors[2].bitmask;
+			*(portPin[2].portRegister) ^= portPin[2].bitmask;
 		}
 	}
+#endif
 	
 	idx = (idx + 1) & 7;
-	int pos = servoPos[idx];
+	int pos = servoPos[idx];	// store volatile variable in a register to avoid unnecessary reads from RAM
 	if (servoSpeed[idx])
 	{
 		if (servoTargetPos[idx] > pos)
@@ -63,6 +69,14 @@ ISR(TIMER1_CAPT_vect)
 }
 
 
+#if !defined (__AVR_ATmega324P__) || !defined (__AVR_ATmega1284P__)
+ISR(TIMER1_COMPA_vect)
+{
+	*(portPin[idx].portRegister) ^= portPin[idx].bitmask;
+}
+#endif
+
+
 int getServoPosition(unsigned char servoNum)
 {
 	return (servoPos[servoNum] + 5) / 10;
@@ -74,6 +88,10 @@ void setServoTarget(unsigned char servoNum, unsigned int pos_us)
 {
 	if (pos_us > 2450)			// will get bad results if pulse is 100% duty cycle (2500)
 		pos_us = 2450;
+#if !defined (__AVR_ATmega324P__) && !defined (__AVR_ATmega1284P__)
+	if (pos_us < 400)
+		pos_us = 400;
+#endif
 	TIMSK1 &= ~(1 << ICIE1);	// make sure we don't get interrupted in the middle of an update	
 	servoTargetPos[servoNum & 7] = pos_us * 10;
 	TIMSK1 |= 1 << ICIE1;
@@ -91,7 +109,7 @@ int getServoTarget(unsigned char servoNum)
 // every 20 ms.
 void setServoSpeed(unsigned char servoNum, unsigned int speed)
 {
-	if (speed > 25000)
+	if (speed > 25000)			// limit speed so it won't cause overflow problems when added to position
 		speed = 25000;
 	TIMSK1 &= ~(1 << ICIE1);	// make sure we don't get interrupted in the middle of an update	
 	servoSpeed[servoNum & 7] = speed;
@@ -122,28 +140,30 @@ struct IOStruct getIORegisters(unsigned char pin)
 	io.ddrRegister = 0;
 	io.bitmask = 0;
 
-	if (pin < 8)
+	if (pin < 8)			// pin 0 = PD0, ..., 7 = PD7
 	{
 		io.pinRegister = (unsigned char*)&PIND;
 		io.portRegister = (unsigned char*)&PORTD;
 		io.ddrRegister = (unsigned char*)&DDRD;
 		io.bitmask = 1 << pin;
 	}
-	else if (pin < 16)
+
+#if defined (__AVR_ATmega324P__) || defined (__AVR_ATmega1284P__)
+	else if (pin < 16)		// pin 8 = PB0, ..., 15 = PB7
 	{
 		io.pinRegister = (unsigned char*)&PINB;
 		io.portRegister = (unsigned char*)&PORTB;
 		io.ddrRegister = (unsigned char*)&DDRB;
 		io.bitmask = 1 << (pin - 8);
 	}
-	else if (pin < 24)
+	else if (pin < 24)		// pin 16 = PC0, ..., 23 = PC7
 	{
 		io.pinRegister = (unsigned char*)&PINC;
 		io.portRegister = (unsigned char*)&PORTC;
 		io.ddrRegister = (unsigned char*)&DDRC;
 		io.bitmask = 1 << (pin - 16);
 	}
-	else if (pin < 32)
+	else if (pin < 32)		// pin 24 = PA7, ..., 31 = PA0
 	{
 		io.pinRegister = (unsigned char*)&PINA;
 		io.portRegister = (unsigned char*)&PORTA;
@@ -151,34 +171,67 @@ struct IOStruct getIORegisters(unsigned char pin)
 		io.bitmask = 1 << (31 - pin);
 	}
 
+#else
+
+	else if (pin < 14)		// pin 8 = PB0, ..., 13 = PB5 (PB6 and PB7 reserved for external clock)
+	{
+		io.pinRegister = (unsigned char*)&PINB;
+		io.portRegister = (unsigned char*)&PORTB;
+		io.ddrRegister = (unsigned char*)&DDRB;
+		io.bitmask = 1 << (pin - 8);
+	}
+	else if (pin < 20)		// pin 14 = PC0, ..., 19 = PC5 (PC6 is reset, PC7 doesn't exist)
+	{
+		io.pinRegister = (unsigned char*)&PINC;
+		io.portRegister = (unsigned char*)&PORTC;
+		io.ddrRegister = (unsigned char*)&DDRC;
+		io.bitmask = 1 << (pin - 14);
+	}
+#endif
+
 	return io;
 }
 
 
-
-
-void initMuxSelector(unsigned char selector, unsigned char pin)
+void initPortPin(unsigned char pinIdx, unsigned char pin)
 {
 	struct IOStruct io = getIORegisters(pin);
 	*(io.portRegister) &= ~io.bitmask;			// PORT pin -> low output or high-impedance input
 	*(io.ddrRegister) |= io.bitmask;			// pin -> output
-	muxSelectors[selector].pinRegister = io.pinRegister;
-	muxSelectors[selector].bitmask = io.bitmask;
+	portPin[pinIdx].portRegister = io.portRegister;
+	portPin[pinIdx].bitmask = io.bitmask;
 }
 
 
+#if defined (__AVR_ATmega324P__) || defined (__AVR_ATmega1284P__)
 void initServos(unsigned char SA, unsigned char SB, unsigned char SC)
 {
-	initMuxSelector(0, SA);
-	initMuxSelector(1, SB);
-	initMuxSelector(2, SC);
-	
-	idx = 0;
+	initPortPin(0, SA);
+	initPortPin(1, SB);
+	initPortPin(2, SC);
 
 	PORTD &= ~(1 << PORTD5);	// MUX output pin -> low output or high-impedance input
 	DDRD |= 1 << PORTD5;		// pin -> output
 
 	TCCR1A = 0b10000010;		// clear OC1A on comp match when upcounting, set OC1A on comp match when downcounting
+
+#else
+void initServos(unsigned char S0, unsigned char S1, unsigned char S2, unsigned char S3,
+				unsigned char S4, unsigned char S5, unsigned char S6, unsigned char S7)
+{
+	initPortPin(0, S0);
+	initPortPin(1, S1);
+	initPortPin(2, S2);
+	initPortPin(3, S3);
+	initPortPin(4, S4);
+	initPortPin(5, S5);
+	initPortPin(6, S6);
+	initPortPin(7, S7);
+
+#endif	
+	idx = 0;
+
+	TCCR1A = 0b00000010;		// disconnect OC1A and OC1B, configure for phase correct PWM (with TCCR1B)
 	TCCR1B = 0b00010001;		// phase correct PWM with TOP = ICR1, clock prescaler = 1 (freq = FCPU / (2 * ICR1))
 
 	ICR1 = 25000;				// 400 Hz PWM (2.5 ms period)
@@ -190,7 +243,7 @@ void initServos(unsigned char SA, unsigned char SB, unsigned char SC)
 
 int main()
 {
-/*	DDRD |= BUZZER;			// needed while no pulldown on MOSFET line
+	DDRD |= BUZZER;			// needed while no pulldown on MOSFET line
 	PORTD &= ~BUZZER;		// needed while no pulldown on MOSFET line
 	DDRD |= 1 << PORTD5;
 	DDRD |= (1 << PORTD0) | (1 << PORTD1) | (1 << PORTD2);
@@ -221,7 +274,7 @@ int main()
 		setServoTarget(7, 1750 - ((i * 3) & 511));
 		//delay_ms(2);
 		i = (i + 1) & 511;
-	}*/
+	}
 }
 
 
