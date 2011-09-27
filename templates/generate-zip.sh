6f9f42e536c25c8c3ec8c2a8859723253465802b
David (Pololu) <dev-david@pololu.com>
2011-09-27 16:20:31 -0700

    Changed the SVP templates to use a general template.avrgccproj.

--- templates/generate-zip.sh
+++ templates/generate-zip.sh
@@ -9,8 +9,10 @@
 set -ue
 
 rm -f *.zip
-zip -jq svp-324.zip svp/svp-324.vstemplate svp/*.c svp/*.avrgccproj svp/*.jpg
-zip -jq svp-1284.zip svp/svp-1284.vstemplate svp/*.c svp/*.avrgccproj svp/*.jpg
+zip -jq svp-324.zip svp/svp-324.vstemplate svp/*.c template.avrgccproj svp/*.jpg
+zip -jq svp-1284.zip svp/svp-1284.vstemplate svp/*.c template.avrgccproj svp/*.jpg
 
 zip -jq x2-1284.zip x2/x2-1284.vstemplate x2/*.c x2/*.avrgccproj x2/*.jpg
 zip -jq x2-644.zip x2/x2-644.vstemplate x2/*.c x2/*.avrgccproj x2/*.jpg
+
+#zip -jq 3pi-328.zip 3pi/3pi-328.vstemplate 3pi/*.c template.avrgccproj 3pi/*.jpg
