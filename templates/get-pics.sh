43a3dad0bd4c7d4543d2339f7b91a2f88f34f4b8
David (Pololu) <dev-david@pololu.com>
2011-09-28 14:54:17 -0700

    templates: Improved the preview picture for the X2 and Baby Orangutan.  Fixed the preview picture for the SV and LV.

--- templates/get-pics.sh
+++ templates/get-pics.sh
@@ -10,17 +10,17 @@ pic 0J1893.32.jpg  > svp/svp_icon.jpg
 pic 0J1882.200.jpg > svp/svp_preview.jpg
 
 pic 0J103.32.jpg   > x2/x2_icon.jpg
-pic 0J103.200.jpg  > x2/x2_preview.jpg
+pic 0J1287.200.jpg  > x2/x2_preview.jpg
 
 pic 0J2415.32.jpg  > 3pi/3pi_icon.jpg
 pic 0J2416.200.jpg > 3pi/3pi_preview.jpg
 
 pic 0J1279.32.jpg  > sv/sv_icon.jpg
-pic 0J1139.200.jpg > sv/sv-168_preview.jpg
-pic 0J1280.200.jpg > sv/sv-328_preview.jpg
+pic 0J1139.200.jpg > sv/sv_168_preview.jpg
+pic 0J1280.200.jpg > sv/sv_328_preview.jpg
 
 pic 0J64.32.jpg    > lv/lv_icon.jpg
 pic 0J65.200.jpg   > lv/lv_preview.jpg
 
 pic 0J661.32.jpg   > baby/baby_icon.jpg
-pic 0J661.200.jpg  > baby/baby_preview.jpg
+pic 0J666.200.jpg  > baby/baby_preview.jpg
