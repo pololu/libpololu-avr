56007706ce9eb661a3b602ac7c650b9a7b6f107b
David (Pololu) <dev-david@pololu.com>
2011-09-30 16:48:49 -0700

    Added a note to install.bat making it clear that it doesn't install the library into the AVR Studio 5 toolchain.  The recommended way to install the library in Windows is use the executable installer.

--- install.bat
+++ install.bat
@@ -2,6 +2,7 @@
 
 @make show_prefix
 @echo (For Windows 7/Vista: make sure to right-click and select "Run as administrator")
+@echo (This script does not support installing the library into AVR Studio 5.)
 @pause
 
 @make install
