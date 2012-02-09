6b760fbe0a363453ea086c9ca044dfcdb2271f4c
David (Pololu) <dev-david@pololu.com>
2012-02-08 17:13:06 -0800

    examples_templates/prepare.sh: Fixed the problem with there being a bare '-D' argument in the AVR Studio 4 examples for all devices except the X2.

--- examples_templates/prepare.sh
+++ examples_templates/prepare.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ue
+
+# This allows the substiatution of AS4_DSM to work.
+IFS=,
 
 example=$1
 device=$2
@@ -19,10 +22,15 @@ cp -a $example_template/*.[ch] $example_dir/
 cat examples_templates/template_$device.mk $example_template/Makefile > $example_dir/Makefile
 
 # Copy and edit the AVR Studio 4 project file.
-cat $example_template/*.aps \
-	| sed "s/<PART>[^<]*<\/PART>/<PART>$mcu<\/PART>/" \
-	| sed "s/<LIB>libpololu[^<]*\.a<\/LIB>/<LIB>libpololu_$device.a<\/LIB>/" \
-	| sed "s/<OPTIONSFORALL>/<OPTIONSFORALL>-D$device_specific_macro /" \
+AS4_DSM=
+if [ $device_specific_macro ]
+then
+  AS4_DSM="s/<OPTIONSFORALL>/<OPTIONSFORALL>-D$device_specific_macro /"
+fi
+cat $example_template/*.aps | sed \
+	-e "s/<PART>[^<]*<\/PART>/<PART>$mcu<\/PART>/" \
+	-e "s/<LIB>libpololu[^<]*\.a<\/LIB>/<LIB>libpololu_$device.a<\/LIB>/" \
+	-e "$AS4_DSM" \
 	> $example_dir/$example.aps
 
 # Copy and edit the AVR Studio 5 project file.
