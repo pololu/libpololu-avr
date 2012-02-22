5941d03951bfc19f3c0016323fcd3109d875cf9d
David (Pololu) <dev-david@pololu.com>
2012-02-21 16:17:40 -0800

    examples: Updated the project file from AVR Studio 5.0 format to AVR Studio 5.1 format.

--- examples_templates/prepare.sh
+++ examples_templates/prepare.sh
@@ -36,11 +36,11 @@ if [ $device_specific_macro ]
 then
   AS5_DSM="s/<avrgcc.compiler.symbols.DefSymbols><ListValues><\/ListValues><\/avrgcc.compiler.symbols.DefSymbols>/<avrgcc.compiler.symbols.DefSymbols><ListValues><Value>$device_specific_macro<\/Value><\/ListValues><\/avrgcc.compiler.symbols.DefSymbols>/"
 fi
-sed < examples_templates/test.avrgccproj \
+sed < examples_templates/test.cproj \
   -e "s/<avrdevice>[^<]*<\/avrdevice>/<avrdevice>$mcu<\/avrdevice>/" \
   -e "s/<Value>pololu_[^<]*<\/Value>/<Value>pololu_$device<\/Value>/" \
   -e "$AS5_DSM" \
-  > $example_dir/$example.avrgccproj
+  > $example_dir/$example.cproj
 
 # Copy and edit the AVR Studio 5.0 solution file.
 sed < examples_templates/test.avrsln \
